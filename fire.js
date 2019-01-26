ecfafaa96debd7bd9975b8fdb4b8d41e8bfc5219
Judar Lima <judar.rslima@gmail.com>
2019-01-26 11:35:42 -0200

    Create js file to with the basic fire logic.

---
+++ fire.js
@@ -0,0 +1,44 @@
+const firePixelsArray = []
+let fireWidth = 10
+let fireHeight = 10
+
+function start() {
+  createFireDataStructure()
+  renderFire()
+}
+
+function createFireDataStructure() {
+  const numberOfPixels = fireWidth * fireHeight
+
+  for (let i = 0; i < numberOfPixels; i++) {
+    firePixelsArray[i] = 0
+  }
+}
+
+function calculateFirePropagation() {
+}
+
+
+function renderFire() {
+  let html = '<table cellpadding=0 cellspacing=0>'
+
+  for (let row = 0; row < fireHeight; row++) {
+    html += '<tr>'
+
+    for (let column = 0; column < fireWidth; column++) {
+      const pixelIndex = column + ( fireWidth * row )
+
+      html += '<td>'
+      html += `<div class="pixel-index">${pixelIndex}</div>`
+      html += '</td>'
+    }
+
+    html += '</tr>'
+  }
+
+  html += '</table>'
+
+  document.querySelector('#fireCanvas').innerHTML = html
+}
+
+start()
